2c93a72e864ceaf72740ecfa8c98fa903ae458aa
Luis Duran <gox@goxbit.dev>
2024-11-28 01:06:16 -0600

    CustomBinding

---
+++ ModelValidationExample/ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.CustomModelBinders;
+using ModelValidationExample.Models;
+
+namespace ModelValidationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        //[Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))]
+        //[ModelBinder(BinderType = typeof(PersonModelBinder))]
+        //[FromBody]
+        public IActionResult Index(Person person, [FromHeader(Name = "User-Agent")] string UserAgent)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(errors);
+            }
+            return Content($"{person}, {UserAgent}");
+        }
+    }
+}
---
+++ ModelValidationExample/ModelValidationExample/CustomModelBinders/PersonModelBinder.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ModelValidationExample.Models;
+
+namespace ModelValidationExample.CustomModelBinders
+{
+    public class PersonModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            Person person = new Person();
+            //FirstName and LastName
+            if (bindingContext.ValueProvider.GetValue("FirstName").Count() > 0)
+            {
+                person.PersonName = bindingContext.ValueProvider.GetValue("FirstName").FirstValue;
+                if (bindingContext.ValueProvider.GetValue("LastName").Count() > 0)
+                {
+                    person.PersonName += " " + bindingContext.ValueProvider.GetValue("LastName").FirstValue;
+                }
+            }
+
+            //Email
+            if (bindingContext.ValueProvider.GetValue("Email").Count() > 0)
+            {
+                person.Email = bindingContext.ValueProvider.GetValue("Email").FirstValue;
+            }
+
+            //Phone
+            if (bindingContext.ValueProvider.GetValue("Phone").Count() > 0)
+            {
+                person.Phone = bindingContext.ValueProvider.GetValue("Phone").FirstValue;
+            }
+
+            //Price
+            if (bindingContext.ValueProvider.GetValue("Price").Count() > 0)
+            {
+                person.Price = Convert.ToDouble(bindingContext.ValueProvider.GetValue("Price").FirstValue);
+            }
+
+            //Password
+            if (bindingContext.ValueProvider.GetValue("Password").Count() > 0)
+            {
+                person.Password = bindingContext.ValueProvider.GetValue("Password").FirstValue;
+            }
+
+            //ConfirmPassword
+            if (bindingContext.ValueProvider.GetValue("ConfirmPassword").Count() > 0)
+            {
+                person.ConfirmPassword = bindingContext.ValueProvider.GetValue("ConfirmPassword").FirstValue;
+            }
+
+            //DateOfBirth
+            if (bindingContext.ValueProvider.GetValue("DateOfBirth").Count() > 0)
+            {
+                person.DateOfBirth = Convert.ToDateTime(bindingContext.ValueProvider.GetValue("DateOfBirth").FirstValue);
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(person);
+            return Task.CompletedTask;
+        }
+    }
+}
